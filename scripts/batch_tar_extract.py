ea1203fe1b241d62a46707e7da99ba25d374863c
Yifei Zhao <yifei@sjtu.edu.cn>
2023-08-30 14:09:58 +0800

    multithread extract

--- scripts/batch_tar_extract.py
+++ scripts/batch_tar_extract.py
@@ -1,24 +1,44 @@
 import os
 import tarfile
+import threading
+from queue import Queue
 
-data_path='./OpenXD-OmniObject3D-New/raw/raw_scans'
+THREADCOUNT=4
+data_path='./OpenXD-OmniObject3D-New/'
+
+q = Queue(maxsize=100)
+threads=[]
+
+def worker(queue,index):
+    while True:
+        file = queue.get()
+        target = file.replace('.tar.gz','')
+        try:
+            print(f'[THREAD-{index}] extracting: '+file)
+            tar = tarfile.open(file,':gz')
+            for extracted_file in tar.getnames():
+                if not os.path.exists(os.path.join(target, extracted_file)):
+                    tar.extract(extracted_file,target)
+                    print(f'[THREAD-{index}] '+ extracted_file)
+                else:
+                    print(f'[THREAD-{index}] skip: ' + extracted_file)
+            tar.close()
+            os.remove(file)
+        except Exception as e:
+            print(f'[THREAD-{index}] '+str(e))
+            print(f'[THREAD-{index}] Fail with: ' + str(file))
+        q.task_done()
+
+
+for i in range(THREADCOUNT):
+    t=threading.Thread(None,target=worker,args=(q,i,),daemon=True)
+    threads.append(t)
+    t.start()
 
 for root, dirs, files in os.walk(data_path):
     for file in files:
         file = os.path.join(root, file)
         if file.endswith('.tar.gz'):
-            target = file.replace('.tar.gz','')
-            try:
-                print('extracting: '+file)
-                tar = tarfile.open(file,':gz')
-                for extracted_file in tar.getnames():
-                    if not os.path.exists(os.path.join(target, extracted_file)):
-                        tar.extract(extracted_file,target)
-                        print(extracted_file)
-                    else:
-                        print('skip: '+extracted_file)
-                tar.close()
-                os.remove(file)
-            except Exception as e:
-                print(e)
-                print("Fail with: " + str(file))
+            q.put(file)
+
+q.join()
