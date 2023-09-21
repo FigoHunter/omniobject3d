6512439a877958b8c0f0ce91172713b8e080978a
FigoHunter <zhaoyifei19950308@hotmail.com>
2023-09-21 14:36:56 +0800

    update

--- scripts/test_trimesh.py
+++ scripts/test_trimesh.py
@@ -9,23 +9,22 @@ def getAffineMat(matrix:np.ndarray) -> np.ndarray:
     affine_mat = np.insert(affine_mat,3,values=[0,0,0,1],axis=0)
     return affine_mat
 
-mesh = trimesh.load(r'Y:\hddisk5\users\yifei\omniobject3d\OpenXD-OmniObject3D-New\raw\decimated\anise\anise_001\Scan\Scan.obj')
-with open(r'Y:\hddisk5\users\yifei\omniobject3d\OpenXD-OmniObject3D-New\raw\decimated\anise\anise_001\Scan\Scan.json','r') as f:
+mesh = trimesh.load(r'Y:\hddisk5\users\yifei\omniobject3d\OpenXD-OmniObject3D-New\raw\decimated\anise\anise_002\Scan\Scan.obj')
+with open(r'Y:\hddisk5\users\yifei\omniobject3d\OpenXD-OmniObject3D-New\raw\decimated\anise\anise_002\Scan\Scan.json','r') as f:
     j = json.load(f)
 
-coord=np.array([[0,0,-1,0],[0,1,0,0],[-1,0,0,0],[0,0,0,1]])
+coord=np.array([[0,0,1,0],[0,1,0,0],[-1,0,0,0],[0,0,0,1]])
+mesh.show()
 
 pos = j['data'][0]['pos']
 rot = j['data'][0]['rot']
-rot=[rot[1],rot[2],rot[3],rot[0]]
 mat = getAffineMat(Rotation.from_quat(rot).as_matrix())
 mat[0][3]=pos[0]
 mat[1][3]=pos[1]
 mat[2][3]=pos[2]
 print(mat)
-mat = np.matmul(np.matmul(coord,mat),np.linalg.inv(coord))
+mat = coord@mat@np.linalg.inv(coord)
 print(mat)
-mesh.show()
 
 mesh.apply_transform(mat)
 mesh.show()
